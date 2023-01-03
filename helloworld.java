24b6ab442f326b0be21e03538953b5726ab782c3
Snigdha-15 <81904275+Snigdha-15@users.noreply.github.com>
2023-01-03 13:24:46 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - Jan 3rd - 1:20 PM");
+    System.out.println("Hello World - Jan 3rd - 1:25 PM");
     }
 }
